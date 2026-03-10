c0017403ad3cd2ee782ba8e172d9c6587081daaf
yukthabillar-netizen <yukthabillar@gmail.com>
2026-03-10 08:47:22 +0530

    Refactor HelloWorld to Example1 with JSON parsing
    Refactored HelloWorld class to Example1 and added JSON parsing functionality.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+package example1.example1;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Example1 {
+    public static void main(String[] args)throws IOException,ParseException, org.json.simple.parser.ParseException {
+        JSONParser jsonparser=new JSONParser();
+        FileReader filereader=new FileReader("W:\\json5\\emp.json");
+        Object obj=jsonparser.parse(filereader);
+        JSONObject empobj=(JSONObject)obj;
+        String fname=(String) empobj.get("firstname");
+        String lname=(String) empobj.get("lastname");
+
+        System.out.println("firstname:"+fname);
+        System.out.println("lastname:"+lname);
     }
 }
